c61d4332a859c4118493699a56e27f107111163b
Maria Tatosh <tatoshmaria@protonmail.com>
2023-05-22 14:16:24 +0300

    Fix migrations

--- database/migrations/2023_05_21_110001_create_words_table.php
+++ database/migrations/2023_05_21_110001_create_words_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\Topic;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,14 +11,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('languages', static function (Blueprint $table) {
+        Schema::create('words', static function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->foreignIdFor(Topic::class)->constrained()->cascadeOnDelete();
+            $table->string('text');
+            $table->char('lang');
+            $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('languages');
+        Schema::dropIfExists('words');
     }
 };
