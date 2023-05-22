f7b747937166e07327d5ab2f1d7e40d9381b0b46
Maria Tatosh <tatoshmaria@protonmail.com>
2023-05-22 22:50:51 +0300

    Remove unused pages

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,5 @@ use Inertia\Inertia;
 
 Route::redirect('/', '/ru');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-
 Route::get('/{lang}', Controllers\DictionaryController::class)->name('dictionary');
 Route::get('/{lang}/api', Controllers\SearchController::class)->name('search');
